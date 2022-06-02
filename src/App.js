a7d13d69520251754f31b0be19dc0394977c18cb
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-02 23:41:40 +0530

    API Setup Successful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+
+// ca1132f
+
+const API_URL = 'http://omdbapi.com?apikey=ca1132f'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('ironman');
+    }, []);
+
     return (
         <h1>Hello World</h1>
     )
