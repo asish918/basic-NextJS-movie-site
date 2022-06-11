e0e0558241ddab5b9c1dd81ad05d320423029ab5
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-12 00:00:06 +0530

    Set up for API request

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -19,6 +21,21 @@ export default function Home() {
       <Navbar />
 
       {/* Results */}
+      <Results results={results}/>
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const query = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[query]?.url || requests.fetchTrending.url}`)
+  .then(res => res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
